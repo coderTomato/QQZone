eb6f11ec7e5ec1e2e770052d839fe4ae66869322
李军 <570547651@qq.com>
2017-02-22 13:05:20 +0800

    添加图片

---
+++ QQZone/QQZone/Classes/Home/ViewController/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  QQZone
+//
+//  Created by 李军 on 2017/2/22.
+//  Copyright © 2017年 李军. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
